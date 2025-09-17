8f488c1591cfb7cff6c9ed10995c7fe55ee5390a
Suraj Benny <surajbeny@gmail.com>
2025-09-17 18:04:12 +0530

    added in homework pullupactivation and maotor_switch_1

---
+++ AVR/DC_motor_switch_1/DC_motor_switch_1/main.c
@@ -0,0 +1,48 @@
+/*
+ * DC_motor_switch_1.c
+ *
+ * Created: 15-09-2025 21:23:48
+ * Author : My PC
+ */ 
+
+
+ 
+#define F_CPU 16000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+int main(void)
+{
+	DDRB|=(1<<DDB5);
+	DDRB|=(1<<DDB4);
+	DDRD&=~(1<<DDD2);
+	while (1)
+	{
+		if(PIND &(1<<PIND2))
+		{
+			
+		
+		PORTB|=(1<<PORTB5);
+		PORTB&=~(1<<PORTB4);
+		_delay_ms(5000);
+		PORTB&=~(1<<PORTB4);
+		PORTB&=~(1<<PORTB5);
+		_delay_ms(1000);
+		PORTB|=(1<<PORTB4);
+		PORTB&=~(1<<PORTB5);
+		_delay_ms(5000);
+		PORTB&=~(1<<PORTB4);
+		PORTB&=~(1<<PORTB5);
+		_delay_ms(1000);
+		}
+		else
+		{
+					PORTB&=~(1<<PORTB4);
+					PORTB&=~(1<<PORTB5);
+					
+					
+		}
+		
+	}
+}
---
+++ AVR/Homework/DC_Motor_switch_pull_up_activation/DC_Motor_switch_pull_up_activation/main.c
@@ -0,0 +1,18 @@
+/*
+ * DC_Motor_switch_pull_up_activation.c
+ *
+ * Created: 17-09-2025 17:19:32
+ * Author : My PC
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
