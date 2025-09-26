b5108bd5c4603d65a83c5a57643771e6eded9a24
Suraj Benny <surajbeny@gmail.com>
2025-09-26 16:46:47 +0530

    modified dc motor_input_delay

--- AVR/Homework/DC_motor_input_delay/DC_motor_input_delay/main.c
+++ AVR/Homework/DC_motor_input_delay/DC_motor_input_delay/main.c
@@ -2,43 +2,48 @@
  * DC_motor_input_delay.c
  *
  * Created: 12-09-2025 16:18:13
- * Author : My PC
- */ 
+ * Author : Suraj
+ */
+
 #define F_CPU 16000000UL
 #include <avr/io.h>
 #include <util/delay.h>
 
-void cw(void t)
-{
-	PORTB|=(1<<PORTB5);
-	PORTB&=~(1<<PORTB4);
-	_delay_ms(t);
-	
-}
-
-void stp()
-{
-	PORTB&=~(1<<PORTB4);
-	PORTB&=~(1<<PORTB5);
-	_delay_ms(1000);
-}
-
-/*void ccw(int t)
-{
-	PORTB|=(1<<PORTB4);
-	PORTB&=~(1<<PORTB5);
-	_delay_ms(t);
-}*/
-int main(void)
-{
-    DDRB|=(1<<DDB4);
-	DDRB|=(1<<DDB5);
-    while (1) 
-    {
-		cw(3000);
-		stp(1000);
-		//ccw(7000);
-		//stp(1000);
+// Custom delay function using 1 ms steps
+void delay_user(int t) {
+    for (int i = 0; i < t; i++) {
+        _delay_ms(1);
     }
 }
 
+
+void cw(int t) {
+    PORTB |= (1 << PORTB5);
+    PORTB &= ~(1 << PORTB4);
+    delay_user(t);
+}
+
+
+void stp(int t) {
+    PORTB &= ~(1 << PORTB4);
+    PORTB &= ~(1 << PORTB5);
+    delay_user(t);
+}
+
+
+void ccw(int t) {
+    PORTB |= (1 << PORTB4);
+    PORTB &= ~(1 << PORTB5);
+    delay_user(t);
+}
+
+int main(void) {
+    DDRB |= (1 << DDB4) | (1 << DDB5);  
+
+    while (1) {
+        cw(3000);     
+        stp(1000);    
+        ccw(7000);    
+        stp(1000);    
+    }
+}
