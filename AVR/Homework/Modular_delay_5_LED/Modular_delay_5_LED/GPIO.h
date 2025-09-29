7c5fff25d26a6bffa4c867c35638c72c0aa083ed
Suraj Benny <surajbeny@gmail.com>
2025-09-29 19:53:11 +0530

    added Modular_delay_5_LED in homework

---
+++ AVR/Homework/Modular_delay_5_LED/Modular_delay_5_LED/GPIO.h
@@ -0,0 +1,19 @@
+/*
+ * GPIO.h
+ *
+ * Created: 29-09-2025 17:44:05
+ *  Author: My PC
+ */ 
+
+
+#ifndef GPIO_H_
+#define GPIO_H_
+
+void delay(uint16_t ms);
+void timer_0_init(void);
+
+
+
+
+
+#endif /* GPIO_H_ */
